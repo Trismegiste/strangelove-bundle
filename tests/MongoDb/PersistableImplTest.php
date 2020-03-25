3466ba5588e6a636c020fbe86b95b08dbd5fe7a3
Trismegiste <flo@trismegiste.net>
2020-03-25 08:01:44 +0100

    complex object persistance ok

--- tests/MongoDb/PersistableImplTest.php
+++ tests/MongoDb/PersistableImplTest.php
@@ -7,6 +7,7 @@
 use MongoDB\BSON\ObjectId;
 use Tests\Fixtures\Hadron;
 use Tests\Fixtures\Lepton;
+use Tests\Fixtures\Nucleus;
 use Tests\Fixtures\Quark;
 use Tests\Toolbox\MongoDb\MongoTestable;
 
@@ -28,6 +29,19 @@ class PersistableImplTest extends MongoTestable {
         $fromDb = $this->resetWriteAndRead($doc);
         $this->assertEquals("proton", $fromDb->getName());
         $this->assertEquals(1.0, $fromDb->getElectricCharge());
+        $this->assertValidMongoId($fromDb->_id);
+    }
+
+    public function testComplexWithoutRoot() {
+        $up = new Quark('up', 2 / 3);
+        $down = new Quark('down', -1 / 3);
+        $proton = new Hadron('proton', [$up, $up, $down]);
+        $neutron = new Hadron('neutron', [$up, $down, $down]);
+        $helion = new Nucleus([$proton, $proton, $neutron, $neutron]); // note : this is reference
+
+        $fromDb = $this->resetWriteAndRead($helion);  // note : this is copy
+        $this->assertEquals(2.0, $fromDb->getElectricCharge());
+        $this->assertEquals(2, $fromDb->getAtomicNumber());
         $this->assertValidMongoId($fromDb->_id);
     }
 
--- tests/fixtures/Nucleus.php
+++ tests/fixtures/Nucleus.php
@@ -12,12 +12,34 @@ use Trismegiste\Toolbox\MongoDb\PersistableImpl;
 /**
  * Description of Nucleus
  */
-class Nucleus implements Persistable {
+class Nucleus implements Persistable, ElectricCharge {
 
     use PersistableImpl;
 
-    public function __construct(array $baryon) {
-        ;
+    protected $baryon;
+
+    public function __construct(array $hadron) {
+        $this->baryon = $hadron;
+    }
+
+    public function getAtomicNumber(): int {
+        $cnt = 0;
+        foreach ($this->baryon as $nucleon) {
+            if ($nucleon->getName() === 'proton') {
+                $cnt++;
+            }
+        }
+
+        return $cnt;
+    }
+
+    public function getElectricCharge(): float {
+        $charge = 0;
+        foreach ($this->baryon as $nucleon) {
+            $charge += $nucleon->getElectricCharge();
+        }
+
+        return $charge;
     }
 
 }
