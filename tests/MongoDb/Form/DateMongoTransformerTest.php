255d2db2e0ef78def25177d47c3dbbf053543ce5
Trismegiste <flo@trismegiste.net>
2022-04-01 11:31:48 +0200

    full tests for MongoDateType

--- tests/MongoDb/Form/DateMongoTransformerTest.php
+++ tests/MongoDb/Form/DateMongoTransformerTest.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\Form\Test\TypeTestCase;
 use Trismegiste\Toolbox\MongoDb\Form\MongoDateType;
+use Trismegiste\Toolbox\MongoDb\Type\MongoDateTime;
 
 class DateMongoTransformerTest extends TypeTestCase
 {
@@ -36,6 +37,25 @@ class DateMongoTransformerTest extends TypeTestCase
         $form->submit(null);
         $this->assertTrue($form->isSynchronized(), $form->getErrors(true, true));
         $this->assertEquals(['year' => '', 'month' => '', 'day' => ''], $form->getData());
+    }
+
+    public function testCreateObjectWithDate()
+    {
+        $form = $this->factory->create(MongoDateType::class);
+        $form->submit(['year' => 2022, 'month' => 2, 'day' => 22]);
+        $this->assertTrue($form->isSynchronized(), $form->getErrors(true, true));
+        $this->assertInstanceOf(MongoDateTime::class, $form->getData());
+        $this->assertEquals(new MongoDateTime('2022-02-22'), $form->getData());
+    }
+
+    public function testViewWithDate()
+    {
+        $form = $this->factory->create(MongoDateType::class, new MongoDateTime('2022-04-01'));
+        $view = $form->createView();
+
+        $this->assertEquals(2022, $view->children['year']->vars['value']);
+        $this->assertEquals(4, $view->children['month']->vars['value']);
+        $this->assertEquals(1, $view->children['day']->vars['value']);
     }
 
 }
