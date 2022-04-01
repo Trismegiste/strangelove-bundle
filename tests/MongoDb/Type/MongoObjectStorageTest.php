ebd56f03da9e4d204d9bcab7aecd7645c77e81d1
Trismegiste <flo@trismegiste.net>
2022-04-01 14:23:44 +0200

    more unit test

--- tests/MongoDb/Type/MongoObjectStorageTest.php
+++ tests/MongoDb/Type/MongoObjectStorageTest.php
@@ -1,17 +1,21 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+use Trismegiste\Toolbox\MongoDb\Type\MongoDateTime;
+use Trismegiste\Toolbox\MongoDb\Type\MongoObjectStorage;
+
 /*
  * Toolbox
  */
 
-class MongoObjectStorageTest extends \PHPUnit\Framework\TestCase
+class MongoObjectStorageTest extends TestCase
 {
 
     protected $sut;
 
     protected function setUp(): void
     {
-        $this->sut = new Trismegiste\Toolbox\MongoDb\Type\MongoObjectStorage();
+        $this->sut = new MongoObjectStorage();
     }
 
     public function testEmpty()
@@ -21,7 +25,7 @@ class MongoObjectStorageTest extends \PHPUnit\Framework\TestCase
 
     public function testOneItem()
     {
-        $obj = new \stdClass();
+        $obj = new stdClass();
         $this->sut[$obj] = 123;
         $this->assertCount(1, $this->sut);
         $this->assertEquals(123, $this->sut[$obj]);
@@ -29,7 +33,7 @@ class MongoObjectStorageTest extends \PHPUnit\Framework\TestCase
 
     public function testSerialize()
     {
-        $obj = new \stdClass();
+        $obj = new stdClass();
         $this->sut[$obj] = 123;
 
         $dump = \MongoDB\BSON\toJSON(\MongoDB\BSON\fromPHP($this->sut));
@@ -47,6 +51,29 @@ class MongoObjectStorageTest extends \PHPUnit\Framework\TestCase
         $obj->rewind();
         $this->assertInstanceOf(stdClass::class, $obj->current());
         $this->assertEquals(123, $obj->getInfo());
+    }
+
+    public function testSerializeCombo()
+    {
+        $obj = new MongoDateTime('1997-12-25');
+        $this->sut[$obj] = 123;
+
+        $dump = \MongoDB\BSON\toJSON(\MongoDB\BSON\fromPHP($this->sut));
+        $this->assertJson($dump);
+
+        return $dump;
+    }
+
+    /** @depends testSerializeCombo */
+    public function testUnserializeCombo(string $json)
+    {
+        $obj = \MongoDB\BSON\toPHP(MongoDB\BSON\fromJSON($json));
+
+        $this->assertCount(1, $obj);
+        $obj->rewind();
+        $this->assertInstanceOf(MongoDateTime::class, $obj->current());
+        $this->assertEquals(123, $obj->getInfo());
+        $this->assertEquals(1997, $obj->current()->format('Y'));
     }
 
 }
