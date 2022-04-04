3de99b49d002a604a6aef67f2e97d7d505d44c16
Trismegiste <flo@trismegiste.net>
2022-04-04 13:02:47 +0200

    cfg load ok

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Strangelove
+ */
+
+namespace Trismegiste\Strangelove\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * The config
+ */
+class Configuration implements ConfigurationInterface
+{
+
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('trismegiste_strangelove');
+
+        $treeBuilder->getRootNode()
+                ->children()
+                    ->arrayNode('mongodb')
+                        ->children()
+                            ->scalarNode('url')
+                                ->defaultValue('mongodb://localhost:27017')
+                            ->end()
+                            ->scalarNode('dbname')->end()
+                        ->end()
+                    ->end()
+                ->end()
+        ;
+
+        return $treeBuilder;
+    }
+
+}
---
+++ src/DependencyInjection/TrismegisteStrangeloveExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Strangelove bundle
+ */
+
+namespace Trismegiste\Strangelove\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * Config extension
+ */
+class TrismegisteStrangeloveExtension extends Extension
+{
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+        
+        var_dump($config);
+    }
+
+}
